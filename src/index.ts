ad9564f15a40cef90a9fb8d1f6e51de2687ea11d
Emrys Corbin <emrys.corbin@gmail.com>
2025-01-12 23:51:59 -0600

    adjust cors, disable unused routes for the time being.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
+import articleRouter from "./routes/articles.ts";
+import skillsRouter from "./routes/skills.ts";
+import projectRouter from "./routes/projects.ts";
+import contentRouter from "./routes/content.ts";
+import { isAdmin } from "./middleware/auth.ts";
+
 const app = new Hono();
 
-app.use("/*", cors());
+app.use(
+  "/*",
+  cors({
+    origin: ["https://corbin.dev"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-// app.use("/*/admin/*", isAdmin());
-//
-// app.route("/articles", articleRouter);
-// app.route("/skills", skillsRouter);
-// app.route("/projects", projectRouter);
-// app.route("/content", contentRouter);
+app.use("/*/admin/*", isAdmin());
+
+app.route("/articles", articleRouter);
+app.route("/skills", skillsRouter);
+app.route("/projects", projectRouter);
+app.route("/content", contentRouter);
 
 app.get("ping", async (c) => {
   return c.text("pong");
